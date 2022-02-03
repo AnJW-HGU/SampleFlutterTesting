187c0ad7a1ebe5707de18c6077ff2a858694d5f2
AnJW-HGU <22000404@handong.edu>
2022-02-03 17:09:27 +0900

    update: Edit test code

--- UnitTest/test.dart
+++ UnitTest/test.dart
@@ -1,20 +1,29 @@
-import 'package:test/test.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+void main() async {
+  await group(
+    'This is a Flutter test group',
+    [
+      testWidgets(
+        'Simple success',
+        MyApp(),
+        (controller) async {
+          await expect(find.text('0'), findsOneWidget, hints: [
+            PossibleHint(findsNothing,
+                'There should be a Text widget that says "0" after the first build.'),
+          ]);
+          await controller.tap(find.byType(RaisedButton));
+          await controller.pump(Duration(milliseconds: 1));
+          await expect(find.text('1'), findsOneWidget, hints: [
+            PossibleHint(findsNothing,
+                'There should be a Text widget that says "1" after the button is pressed.'),
+          ]);
+        },
+      ),
+    ],
+    reporter: (results) {
+      _result(
+        !results.any((r) => !r.success),
+        results.map((r) => r.messages ?? []).expand((i) => i).toList(),
+      );
+    },
+  );
 }
