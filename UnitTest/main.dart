26cd08773c9cba92d8f5a52cc4e642be667bd002
AnJW-HGU <22000404@handong.edu>
2022-02-03 16:08:28 +0900

    update: Create test.dart, solution.dart, yaml file

--- UnitTest/main.dart
+++ UnitTest/main.dart
@@ -1,21 +1,18 @@
 // Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Unit Test',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Counter App Unit Test'),
     );
   }
 }
@@ -56,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
+              key: const Key('counter'),
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
@@ -63,6 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        key: const Key('increment'),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
---
+++ UnitTest/solution.dart
@@ -0,0 +1,2 @@
+/// 이 코드는 Flutter Unit Test를 위한 것입니다.
+/// test.dart 파일을 show로 선택하여 확인해주세요.
