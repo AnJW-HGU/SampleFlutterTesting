50d3e7296d15947874f5c3bc6776f3225ffaf979
Salgu <22000404@handong.edu>
2022-02-18 17:35:14 +0900

    Update test.dart

--- WidgetTest/test.dart
+++ WidgetTest/test.dart
@@ -1,12 +1,30 @@
 void main() {
-  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
-    await tester.pumpWidget(MyWidget(title: 'T', message: 'M'));
-    final titleFinder = find.text('T');
-    final messageFinder = find.text('M');
+  testWidgets('drag 2', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.pump();
+    await tester.drag(find.byKey(Key('2')), Offset(500.0, 0.0));
+    await tester.pumpAndSettle();
+    
+    expect(find.text('2'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+  
+  testWidgets('drag 99', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.pump();
 
-    // Use the `findsOneWidget` matcher provided by flutter_test to verify
-    // that the Text widgets appear exactly once in the widget tree.
-    expect(titleFinder, findsOneWidget);
-    expect(messageFinder, findsOneWidget);
+    // Scroll until the item to be found appears.
+    await tester.scrollUntilVisible(
+      find.byKey(Key('99')),
+      500.0,
+      scrollable: find.byType(Scrollable),
+    );
+    
+    await tester.drag(find.byKey(Key('99')), Offset(500.0, 0.0));
+    await tester.pumpAndSettle();
+
+    // Verify that the item contains the correct text.
+    expect(find.text('99'), findsNothing);
+    expect(find.text('98'), findsOneWidget);
   });
 }
