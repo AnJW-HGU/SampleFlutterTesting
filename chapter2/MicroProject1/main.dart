9b1f7c817f13c5db7452547196a8b808bfcb9fdf
Salgu <22000404@handong.edu>
2022-02-18 17:13:27 +0900

    Update main.dart

--- chapter2/MicroProject1/main.dart
+++ chapter2/MicroProject1/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
