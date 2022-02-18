b42911f7fc3b571eabfc6fc38370fd6fee5997e1
Salgu <22000404@handong.edu>
2022-02-18 17:48:56 +0900

    Update test.dart

--- WidgetTest/test.dart
+++ WidgetTest/test.dart
@@ -1,12 +1,54 @@
 void main() {
-  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
-    await tester.pumpWidget(MyWidget(title: 'T', message: 'M'));
-    final titleFinder = find.text('T');
-    final messageFinder = find.text('M');
+  testWidgets('drag 2', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.drag(find.byKey(Key('2')), Offset(500.0, 0.0));
+    await tester.pumpAndSettle();
+    
+    // expect(find.text('2'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
 
-    // Use the `findsOneWidget` matcher provided by flutter_test to verify
-    // that the Text widgets appear exactly once in the widget tree.
-    expect(titleFinder, findsOneWidget);
-    expect(messageFinder, findsOneWidget);
+    final list2Finder = find.text('2').toString();
+    final list1Finder = find.text('1').toString();
+    if (list2Finder != 'zero widgets with text "2" (ignoring offstage widgets)') {
+      _result(false, ['2 is dragged and not deleted.']);
+    }
+    if (list1Finder != 'exactly one widget with text "1" (ignoring offstage widgets): Text("1", dependencies: [DefaultTextStyle, MediaQuery])') {
+      _result(false, ['There is no 1, or more than two']);
+    }
+    // print('${list2Finder}z');
+    // print('${list1Finder}z');
+  });
+  
+  testWidgets('drag 99', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    // Scroll until the item to be found appears.
+    await tester.scrollUntilVisible(
+      find.byKey(Key('99')),
+      500.0,
+      scrollable: find.byType(Scrollable),
+    );
+    
+    await tester.drag(find.byKey(Key('99')), Offset(500.0, 0.0));
+    await tester.pumpAndSettle();
+
+    // Verify that the item contains the correct text.
+    // expect(find.text('99'), findsNothing);
+    // expect(find.text('98'), findsOneWidget);
+
+    final list99Finder = find.text('99').toString();
+    final list98Finder = find.text('98').toString();
+    if (list99Finder != 'zero widgets with text "99" (ignoring offstage widgets)') {
+      _result(false, ['2 is dragged and not deleted.']);
+    }
+    if (list98Finder != 'exactly one widget with text "98" (ignoring offstage widgets): Text("98", dependencies: [DefaultTextStyle, MediaQuery])') {
+      _result(false, ['There is no 1, or more than two']);
+    }
+    // print('${list99Finder}z');
+    // print('${list98Finder}z');
+  });
+  
+  testWidgets('--Test Successful--', (WidgetTester tester) async {
+    _result(true);
   });
 }
